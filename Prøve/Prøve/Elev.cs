1c90bbd1a19b90ae8d11fa22f9cd769cdb1a8ea0
Rosenheim <patrickrosenheim@gmail.com>
2015-09-17 12:18:56 +0200

    Le test

---
+++ Prøve/Prøve/Elev.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Prøve
+{
+    class Elev
+    {
+        public string navn;
+        public bool isDumpet;
+        public float height;
+        public int grade;
+        public string subject;
+
+        public Elev(string navn, bool isDumpet, float height, int grade, string subject)
+        {
+            this.navn = navn;
+            this.isDumpet = isDumpet;
+            this.height = height;
+            this.grade = grade;
+            this.subject = subject;
+        }
+
+        public override string ToString()
+        {
+            var dumpet = isDumpet ? "Ja." : "Nej";
+            return "Navn: " + navn + " Dumpet: " + dumpet + " Højde: " + height + " Karakter: " + grade + " Emne: " + subject;
+        }
+    }
+}
---
+++ Prøve/Prøve/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace Prøve
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region opg1
+
+            /*
+             * SUMMARY:
+             *      Anony metode - retunere true hvis string
+             *      er større en 5
+             */
+            Func<string, bool> isThisStringLongerThanFiveCharactersFunc = (thisIsALongNameThatIcantPossibleUseAgain) =>
+            {
+                return thisIsALongNameThatIcantPossibleUseAgain.Length >= 5;
+            };
+            Console.WriteLine(isThisStringLongerThanFiveCharactersFunc("No")); // Lenght = 2
+            Console.WriteLine(isThisStringLongerThanFiveCharactersFunc("This is")); // Lenght == 7
+
+            #endregion
+            #region opg2
+
+            /*
+             * SUMMARY:
+             *      Bubblesort, anonym metode, compare string on lenght.
+             *      
+             * COMMENT:
+             *      
+             */
+            Func<string, string, bool> areTheseTwoStringsTheSameLenghtFunc = (s1, s2) => s1.Length > s2.Length;
+
+            #endregion
+            Elev elev = new Elev("Peter", false, 1.80F, 12, "Android");
+            Console.WriteLine(elev.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
